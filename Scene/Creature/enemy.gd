f888f602dc36337e40463f90e2307576beaf9607
Hadi <winatahadipratama@gmail.com>
2025-07-05 17:21:01 +0700

    LOL

--- Scene/Creature/enemy.gd
+++ Scene/Creature/enemy.gd
@@ -7,7 +7,6 @@ enum State {
 }
 
 @export var speed: int = 100
-@export var health: int = 1
 
 var player: Player = null
 var player_in_area := false
@@ -25,38 +24,43 @@ func _ready() -> void:
 	state_timer = MOVE_DURATION
 
 func _physics_process(delta: float) -> void:
-	if dead:
-		$EnemyFollowArea/enemy_follow_area.disabled = true
-		velocity = Vector2.ZERO
-		move_and_slide()
-		return
+	if player:
+		if player.player_dead:
+			$PlayerDetector/CollisionShape2D.disabled = true
+			velocity = Vector2.ZERO
+			move_and_slide()
+			return
 	else:
-		$EnemyFollowArea/enemy_follow_area.disabled = false
-
-	if player_in_area and player and can_see_player() and not player_dead:
+		$PlayerDetector/CollisionShape2D.disabled = false
+	
+	if (player 
+		and player_in_area
+		and can_see_player() 
+		and not player.player_dead
+		):
 		var direction: Vector2 = (player.global_position - global_position).normalized()
 		velocity = direction * speed
 	else:
 		handle_random_movement(delta)
-
+	
 	move_and_slide()
 
 func handle_random_movement(delta: float) -> void:
 	state_timer -= delta
-
+	
 	if state == State.MOVING:
 		velocity = random_direction * speed
-
+		
 		if state_timer <= 0:
-			state = State.
+			state = State.IDLE
 			state_timer = IDLE_DURATION
 			velocity = Vector2.ZERO
-
-	elif state == "idle":
+	
+	elif state == State.IDLE:
 		velocity = Vector2.ZERO
-
+	
 		if state_timer <= 0:
-			state = "moving"
+			state = State.MOVING
 			state_timer = MOVE_DURATION
 			random_direction = get_random_direction()
 
@@ -66,28 +70,31 @@ func get_random_direction() -> Vector2:
 
 func can_see_player() -> bool:
 	var ray: RayCast2D = $PlayerRaycast
-	if ray == null or player == null:
+	
+	# Kalau player tersembunyi
+	if ray == null or player == null or player.is_currently_hidden:
 		return false
-
+	
 	var to_player: Vector2 = (player.global_position - global_position).normalized()
 	ray.target_position = to_player * 550  # make sure it reaches past the player
 	ray.force_raycast_update()
-
+	
 	if ray.is_colliding():
 		var hit: Object = ray.get_collider()
 		#print("Ray hit: ", hit.name, " | Type: ", hit.get_class())
-		if hit == player or hit.get_parent() == player:
+		if hit is Player or hit.get_parent() is Player:
 			#print("✔ Player detected!")
 			return true
 	return false
 
 
-func _on_enemy_follow_area_body_entered(body: Node) -> void:
-	if body.has_method("player"):
+func _on_player_detector_body_entered(body: Node2D) -> void:
+	if body is Player:
 		player_in_area = true
-		player = body as CharacterBody2D
+		player = body as Player
 
-func _on_enemy_follow_area_body_exited(body: Node) -> void:
-	if body.has_method("player"):
+
+func _on_player_detector_body_exited(body: Node2D) -> void:
+	if body is Player:
 		player_in_area = false
 		player = null
